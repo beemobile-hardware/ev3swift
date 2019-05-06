410fcdee6d406c9fdc52225ad90798b67986bde7
Piotr Kundu <piotrk@spotify.com>
2019-05-06 12:25:44 +0200

    Multiplatform project for iOS, OSX, WatchOS

---
+++ Sources/HelloWorld.swift
@@ -0,0 +1,8 @@
+//
+//  HelloWorld.swift
+//  ev3swift
+//
+//  Created by Piotr Kundu on 2019-05-06.
+//
+
+import Foundation
---
+++ Sources/ev3swift.h
@@ -0,0 +1,19 @@
+//
+//  ev3swift_osx.h
+//  ev3swift-osx
+//
+//  Created by Piotr Kundu on 2019-05-06.
+//
+
+#import <Foundation/Foundation.h>
+
+
+//! Project version number for ev3swift_osx.
+FOUNDATION_EXPORT double _osx;
+
+//! Project version string for ev3swift_osx.
+FOUNDATION_EXPORT const unsigned char ev3swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ev3swift_osx/PublicHeader.h>
+
+
